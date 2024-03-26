6f5b4b83f3d3806818c7459962e232ee7fade2ca
Oleh Sidun <sidunoleh@gmail.com>
2024-03-26 11:56:15 +0200

    Create parallel.php

---
+++ parallel.php
@@ -0,0 +1,38 @@
+<?php
+
+function parallel(
+    array $tasks, 
+    callable $callback, 
+    int $processes_count
+) {
+    for (
+        $process_numb=0; 
+        $process_numb < $processes_count; 
+        $process_numb++
+    ) { 
+        $pid = pcntl_fork();
+        
+        if ($pid == -1) {
+            throw new Exception('Fork error.');
+        }
+
+        if ($pid == 0) {
+            break;
+        }
+    }
+
+    if ($pid) {
+        for ($i=0; $i < $processes_count; $i++) { 
+            pcntl_wait($status);
+        }
+        return;
+    }
+
+    for (
+        $i=$process_numb; 
+        $i < count($tasks); 
+        $i=$i+$processes_count
+    ) { 
+        $callback($tasks[$i]);
+    }
+}
